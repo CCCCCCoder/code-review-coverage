5cc9cf93e689dac32a068244346d80212289423b
GitHub Action <action@github.com>
2022-06-14 16:16:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ccccccoder/codereviewcoverage/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ccccccoder.codereviewcoverage.services
+
+import com.github.ccccccoder.codereviewcoverage.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
